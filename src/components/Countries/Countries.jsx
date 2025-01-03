9d436cb13a6580f8d69cc24ad7810278894815c8
Saayemm <sayemdcc@gmail.com>
2025-01-04 00:23:08 +0600

    country

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -4,18 +4,35 @@ import Country from "./Country"
 
 export default function Countries() {
     const [countries, setCountry] = useState([])
+    const [visitedCountries, setVisitedCountries] = useState([])
 
     useEffect(()=>{
         fetch("https://restcountries.com/v3.1/all")
         .then(res=>res.json())
         .then(res=>setCountry(res))
     }, [])
+
+    const hundleVisitedCountry = country => {
+      console.log(country)
+      const newVisitedCountry = [...visitedCountries, country]
+      setVisitedCountries(newVisitedCountry)
+    }
   return (
     <div>
         <h3>Countries</h3>
+        <div>
+          <h3>Visited Countries {visitedCountries.length}</h3>
+          <ul>
+            {
+              visitedCountries.map(country=><li key={country.cca3}>{country.name.common}</li>)
+            }
+          </ul>
+        </div>
         <h3>Countries: {countries.length}</h3>
         {
-          countries.map(country=><Country country={country} key={country.cca2}></Country>)
+          countries.map(country=><Country country={country}
+            hundleVisitedCountry={hundleVisitedCountry} 
+            key={country.cca2}></Country>)
         }
     </div>
   )
--- src/components/Countries/Country.jsx
+++ src/components/Countries/Country.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function Country({country}) {
+export default function Country({country, hundleVisitedCountry}) {
 
     const [visited, setVisited] = useState(false)
 
@@ -11,11 +11,13 @@ export default function Country({country}) {
     setVisited(!visited)
    }
   return (
-    <div className="bg-red-100 text-black p-4 m-2 flex justify-between items-center">
+    <div className={`bg-red-100 text-black p-4 m-2 flex justify-between items-center  ${visited && 'visited'}`}>
        <div className="flex flex-col gap-1 justify-start items-start">
         <h3 className="text-xl font-semibold">Name: {name.common}</h3>
         <p>Population: {population}</p>
         <p>Region: {region}</p>
+        <button className="bg-black text-white px-4 py-1 rounded-sm"
+            onClick={()=>hundleVisitedCountry(country)}>Mark as Visited</button>
         <button 
             className="bg-black text-white px-4 py-1 rounded-sm"
             onClick={hundleVisited}>{visited ? "visited" : "Wanna Visit"}</button>
