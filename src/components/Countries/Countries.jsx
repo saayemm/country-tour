1d2f2e4244acb3d975cf99281403b44d3a125c14
Saayemm <sayemdcc@gmail.com>
2025-01-04 00:27:54 +0600

    css

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -18,22 +18,26 @@ export default function Countries() {
       setVisitedCountries(newVisitedCountry)
     }
   return (
-    <div>
+    <div className="flex gap-4">
+      <div>
         <h3>Countries</h3>
-        <div>
-          <h3>Visited Countries {visitedCountries.length}</h3>
-          <ul>
-            {
-              visitedCountries.map(country=><li key={country.cca3}>{country.name.common}</li>)
-            }
-          </ul>
-        </div>
         <h3>Countries: {countries.length}</h3>
         {
           countries.map(country=><Country country={country}
             hundleVisitedCountry={hundleVisitedCountry} 
             key={country.cca2}></Country>)
         }
+      </div>
+       
+        <div className="border-2 border-orange-600 p-2">
+          <h3 className="text-2xl">Visited Countries <span className="text-green-700">{visitedCountries.length}</span></h3>
+          <ul>
+            {
+              visitedCountries.map(country=><li key={country.cca3}>{country.name.common}</li>)
+            }
+          </ul>
+        </div>
+       
     </div>
   )
 }
