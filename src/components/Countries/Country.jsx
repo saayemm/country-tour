0a121586b4cf6a0e384bc5649a6a63d617e3ba3f
Saayemm <sayemdcc@gmail.com>
2025-01-03 23:48:54 +0600

    text-toggle

--- src/components/Countries/Country.jsx
+++ src/components/Countries/Country.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react"
 
 export default function Country({country}) {
+
+    const [visited, setVisited] = useState(false)
+
    console.log(country)
    const {name,flags,population, region} = country
+
+   const hundleVisited = () => {
+    setVisited(!visited)
+   }
   return (
     <div className="bg-red-100 text-black p-4 m-2 flex justify-between items-center">
-       <div className="flex flex-col gap-1">
+       <div className="flex flex-col gap-1 justify-start items-start">
         <h3 className="text-xl font-semibold">Name: {name.common}</h3>
         <p>Population: {population}</p>
         <p>Region: {region}</p>
+        <button 
+            className="bg-black text-white px-4 py-1 rounded-sm"
+            onClick={hundleVisited}>{visited ? "visited" : "Wanna Visit"}</button>
+            {visited ? "Already explore this country" : "I want to explore this country"}
        </div>
 
         <img src={flags.png} alt="" />
